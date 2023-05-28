e06936a93f830d41a9efec31dc0a64a32f8fe1dd
Shangeetha19 <134485776+Shangeetha19@users.noreply.github.com>
2023-05-28 20:35:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var names=new Array();
+names[0]="Shangeetha";
+names[1]="Sassy";
+names[2]="Priya";
+names[3]="Sara";
+names[4]="Hannah";
+names[5]="Rishi";
+names[6]="Suganth";
+names[7]="Shyam";
+names[8]="Leena";
+names[9]="Leah";
+for(var i=0;i<names.length;i++){
+    if(names[i].charAt()=='J'||names.charAt()=='j'){
+    console.log(names[i]);
+    }else{
+        console.log("Hello "+names[i]);
+    }
+}
